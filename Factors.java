cfc40f3da828b3999c9a387fd17dad72bd6cdf6e
Alka05 <53633796+Alka05@users.noreply.github.com>
2020-05-02 18:40:48 +0530

    Add files via upload

---
+++ Factors.java
@@ -0,0 +1,21 @@
+package test;
+
+import java.util.Scanner;
+
+public class Factors {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter number");
+		int number = sc.nextInt();
+		int fact = 1;
+		for (int num = number; num > 0; num--) {
+			fact = fact * num;
+			System.out.println(num);
+
+		}
+
+		System.out.println("factor of num" + " " + fact);
+
+	}
+}
